927d834ca5aa09c3b4cda6b43cf785def03c2be5
Emmanuel Achana <achanamosey@gmail.com>
2020-08-31 18:50:55 +0100

    Added all the files.

---
+++ src/views/app/index.jsx
@@ -0,0 +1,21 @@
+import Chat from "views/app/Chat";
+import Dashboard from "views/app/dashboards/Dashboard";
+import Contact from "views/app/Contact";
+import Scrumboard from "views/app/Scrumboard";
+import Calender from "views/app/Calender";
+import FileManager from "views/app/FileManager";
+import Todos from "views/app/Todos";
+import Analytics from "views/app/dashboards/Analytics";
+import ECommerce from "views/app/dashboards/ECommerce";
+
+export {
+  Chat,
+  Dashboard,
+  Contact,
+  Scrumboard,
+  Calender,
+  FileManager,
+  Todos,
+  Analytics,
+  ECommerce
+};
